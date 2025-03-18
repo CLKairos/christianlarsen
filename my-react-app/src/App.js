4be17f5ed1baec39ff72c069f1e696afdd484610
Christian Larsen <christi9nl9rsen@gmail.com>
2025-03-17 23:32:44 -0500

    Update App.js

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -9,17 +9,17 @@ function App() {
         <p>Christian Larsen</p>
         <p>Class of 2028</p>  
         <p>Software Enginner, and Web Developer</p>
-        <p><a href='https://docs.google.com/document/d/1M2LerwCtYjl00oKIyRdX2ISRRcTVeW3Je0Sme40OYlw/edit?usp=sharing'>Resume</a></p>
+        <p><a href='https://github.com/CLKairos/'>Github</a></p>
       </header>
       <div className='footer'>
         <div className='contact'>
           <p><b>Contact</b></p>
           <p>Email: christi9nl9rsen@gmail.com</p>
-          <p>Phone: (469)-742-3710</p>
           <p>Discord: ka1ros._.</p>
         </div>
         <div className='links'>
           <p><b>Links</b></p>
+          <p><a href='https://kaiross.info'>Business Page</a></p>
           <p><a href='https://youtube.com/@realkairos'>YouTube</a></p>
           <p><a href='https://twitch.tv/realka1ros'>Twitch</a></p>
         </div>
